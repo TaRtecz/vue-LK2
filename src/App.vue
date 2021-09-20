89df05a06c5d971a4e5d8583d1f6af7bb12660bb
John Doe <johndoe@example.com>
2021-09-20 13:08:09 +0600

    add RegForm component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RegForm msg="Registration"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RegForm from './components/RegForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RegForm
   }
 }
 </script>
